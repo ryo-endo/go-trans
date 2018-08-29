286f8afc9fe4e7e9dd16e05dbf58b6def00fcdf6
ryo-endo <to.endo.dev@gmail.com>
2018-08-30 08:18:26 +0900

    Move the translation functions to new file.

---
+++ translator.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func trans(s string, from string, to string, key string) (string, error) {
+	url := fmt.Sprintf("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&from=%s&to=%s", from, to)
+	body := strings.NewReader(fmt.Sprintf("[{'Text':'%s'}]", s))
+
+	resp, err := callTranslateApi(url, body, key)
+	if err != nil {
+		return "", fmt.Errorf("Cannot connect to the api server.\n%s", err.Error())
+	}
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != 200 {
+		var result errorResponse
+		if err := json.Unmarshal(bytes, &result); err != nil {
+			return "", fmt.Errorf("The server returned an invalid JSON response.\n%s", err.Error())
+		}
+		return "", fmt.Errorf("The server returned an error respons.\ncode=%d\nmessage=%s", result.Error.Code, result.Error.Message)
+	}
+
+	var result transResponse
+	if err := json.Unmarshal(bytes, &result); err != nil {
+		return "", fmt.Errorf("The server returned an invalid JSON response.\n%s", err.Error())
+	}
+
+	out := result[0].Translations[0].Text
+	return out, nil
+
+}
+
+func callTranslateApi(url string, body *strings.Reader, key string) (*http.Response, error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = map[string][]string{
+		"Content-Type":              {"application/json"},
+		"Ocp-Apim-Subscription-Key": {key},
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
