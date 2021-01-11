6ab6069926df1d0c40c6508bb1d27ace7ac1a798
ryo-endo <to.endo.dev@gmail.com>
2021-01-11 16:42:22 +0900

    Delete duplicated test. (its flag test)

--- main_test.go
+++ main_test.go
@@ -1,21 +1,14 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestParseEmptyArgs(t *testing.T) {
 	// Empty args
-	args := []string{}
+	args := []string{""}
 	err := Run(args)
 	if err == nil {
 		t.Fatal("Don't return error with the empty args.")
-	}
-}
-
-func TestParseArgsWithUndefinedFlag(t *testing.T) {
-	// undefined flag
-	args := []string{"-z", "err"}
-	err := Run(args)
-	if err == nil {
-		t.Fatal("Don't return error with the undefined flag.")
 	}
 }
